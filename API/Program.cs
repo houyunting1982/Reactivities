d2c55d68ccdbb8c6209bf2607c9754cfadb0258f
houyunting1982 <yhou@servicetitan.com>
2019-12-29 23:45:31 -0800

    End of Section 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,13 +16,14 @@ namespace API
         {
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope()) {
-                var service = scope.ServiceProvider;
+                var services = scope.ServiceProvider;
                 try {
-                    var context = service.GetRequiredService<DataContext>();
+                    var context = services.GetRequiredService<DataContext>();
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
                     context.Database.Migrate();
-                    Seed.SeedData(context);
+                    Seed.SeedData(context, userManager).GetAwaiter().GetResult();
                 } catch (Exception ex) {
-                    var logger = service.GetRequiredService<ILogger<Program>>();
+                    var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration.");
                 }
             }
---
+++ Application/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,8 @@
+using Domain;
+
+namespace Application.Interfaces
+{
+    public interface IJwtGenerator {
+        string CreateToken (AppUser user);
+    }
+}
